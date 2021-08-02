b65132882d516d2d99590aeda3d91641565ebc6c
xwj <1983318249@qq.com>
2021-08-02 21:15:59 +0800

    7.26

--- 2021_7_26/2021_7_26/test.c
+++ 2021_7_26/2021_7_26/test.c
@@ -24,20 +24,42 @@
 //}
 
 //practise：
-#include<stdio.h>
-int main()
-{
-	int a = 10;
-	int b = 20;
-	int sum = 0;
-	sum = a + b;
-	printf("sum = %d\n", sum);
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int sum = 0;
+//	sum = a + b;
+//	printf("sum = %d\n", sum);
+//
+//	//在vs中如果想要正常使用scanf函数，就需要在工程的第一行加上：#define _CRT_SECURE_NO_WARNINGS1
+//	scanf("%d %d", &a, &b);   
+//	sum = a + b;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
 
-	//在vs中如果想要正常使用scanf函数，就需要在工程的第一行加上：#define _CRT_SECURE_NO_WARNINGS1
-	scanf("%d %d", &a, &b);   
-	sum = a + b;
-	printf("sum = %d\n", sum);
-	return 0;
+
+//写一个函数求两个整数的较大值
+#include<stdio.h>
+
+int my_max(int x, int y)
+{
+	if (x > y)
+		return x;
+	else
+		return y;
 }
 
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int Max = 0;
+	scanf("%d %d", &a, &b);
+	Max = my_max(a, b);
+	printf("Max = %d\n", Max);
+	return 0;
+}
 
