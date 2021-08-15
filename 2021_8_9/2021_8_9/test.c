43e8200e7841b0d4dfb792751ba5a0e3da2ab2a4
xwj <1983318249@qq.com>
2021-08-15 22:20:51 +0800

    2021.8.9

--- 2021_8_9/2021_8_9/test.c
+++ 2021_8_9/2021_8_9/test.c
@@ -79,6 +79,7 @@
 //}
 
 
+//题1
 //#include <stdio.h>
 //int main()
 //{
@@ -90,6 +91,7 @@
 //}
 
 
+//题2
 //#include <stdio.h>
 //int main()
 //{
@@ -99,6 +101,7 @@
 //}
 
 
+//题3
 //#include <stdio.h>
 //int main()
 //{
@@ -108,10 +111,85 @@
 //}
 
 
+//#include<stdio.h>
+//int main()
+//{
+//	printf("hello world!\n");
+//	return 0;
+//}
 
+
+//题四
+//#include<stdio.h>
+//int main()
+//{
+//	int i = -20;
+//	unsigned  int  j = 10;
+//	printf("%d\n", i + j);
+//	//按照补码的形式进行运算，最后格式化成为有符号整数
+//	return 0;
+//}
+
+
+//题5
+//#include<stdio.h>
+//#include<windows.h>
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//		Sleep(1000);
+//	}
+//	return 0;
+//}
+
+
+//题6
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//
+//    char a[1000];
+//    int i;
+//    for (i = 0; i < 1000; i++)
+//    {
+//        a[i] = -1 - i;
+//    }
+//    printf("%d", strlen(a));
+//	return 0;
+//}
+
+
+//题7
+//#include<stdio.h>
+//
+//unsigned char i = 0;
+//int main()
+//{
+//	for (i = 0; i <= 255; i++)
+//	{
+//		printf("hello world\n");
+//		//printf("%u\n", i);    //用于查看i的值是不是总是大于等于0。
+//	}
+//
+//	return 0;
+//}
+
+
+//浮点数的存储
 #include<stdio.h>
 int main()
 {
-	printf("hello world!\n");
+	int n = 9;
+	float* pFloat = (float*)&n;
+	printf("n的值为：%d\n", n);
+	printf("*pFloat的值为：%f\n", *pFloat);
+	*pFloat = 9.0;
+	printf("num的值为：%d\n", n);
+	printf("*pFloat的值为：%f\n", *pFloat);
+
 	return 0;
 }
