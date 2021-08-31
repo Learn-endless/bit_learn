853cd4e485d8075a2fd315c842cd521003b3a082
xwj <1983318249@qq.com>
2021-08-31 16:17:46 +0800

    2021.7.19

--- 2021_7_19/2021_7_19/test.c
+++ 2021_7_19/2021_7_19/test.c
@@ -271,44 +271,256 @@
 //}
 
 
+//#include<stdio.h>
+//int my_seek(int* p, int n, int f)
+//{
+//	int left = 0;
+//	int right = n;
+//	while (left < right)        //right指向的位置没有数据，不需要加等号 [left,right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (*(p + mid) < f)
+//		{
+//			left = mid + 1;  //左边的数据去得到，所以 mid + 1
+//		}
+//		else if (*(p + mid) > f)
+//		{
+//			right = mid; //右边的数据取不到,不用-1
+//		}
+//		else
+//		{
+//			return mid;
+//		}
+//	}
+//	return -1;
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int number = sizeof(arr) / sizeof(arr[0]);
+//	int a = 0;
+//	printf("请输入需要查找的数：");
+//	scanf("%d", &a);
+//	int ret = my_seek(arr, number, a);
+//	if (ret != -1)
+//	{
+//		printf("找到了，下标为：%d\n", ret);
+//	}
+//	else
+//	{
+//		printf("没找到！");
+//	}
+//	return 0;
+//}
+
+
+//for循环条件的省略
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (; i < 10; i++)
+//	{
+//		for (; j < 10; j++)         //i = 0时，j从0循环到10，但当i = 1、2、3、4、5、6...时，j都是从10开始，就导致一次都不打印
+//		{
+//			printf("hehe\n");       //这里只会打印10个hehe
+//		}
+//	}
+//	return 0;
+//}
+
+
+//计算n的阶乘
 #include<stdio.h>
-int my_seek(int* p, int n, int f)
+//int main()
+//{
+//	int n = 0;
+//	int i = 0;
+//	int sum = 1;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		sum *= i;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	int n = 0;
+//	int sum = 1;
+//	scanf("%d", &n);
+//	while (i <= n)
+//	{
+//		sum *= i++;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//求1!+2!+3!+4!+...+n!之和
+//int main()
+//{
+//	int i = 1;
+//	int n = 0;
+//	int sum = 1;
+//	int num = 0;
+//	scanf("%d", &n);
+//	while (i <= n)
+//	{
+//		num += sum *= i++;
+//	}
+//	printf("%d\n", num);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int sum = 0;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		int num = 1;
+//		for (j = 1; j <= i; j++)
+//		{
+//			num *= j;
+//		}
+//		sum += num;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//二分查找：左闭右开
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int left = 0;
+//	int right = sizeof(arr) / sizeof(arr[0]);  // 左闭右开时可以从数组最后一个元素的下一个元素开始
+//	int n = 0;                               //需要查找的数据
+//	int mid = 0;
+//	scanf("%d", &n);
+//	while (left < right)    //左闭右开时不用加等号
+//	{
+//		mid = (left + right) / 2;
+//		if (arr[mid] < n)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > n)
+//		{
+//			right = mid;
+//		}
+//		else
+//		{
+//			break;
+//		}
+//	}
+//	if (left < right)
+//	{
+//		printf("找到了，下标为：%d\n", mid);
+//	}
+//	else
+//	{
+//		printf("没找到!");
+//	}
+//	return 0;
+//}
+
+//左闭右闭
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int left = 0;
+//	int right = sizeof(arr) / sizeof(arr[0]);  // 左闭右闭从数组最后一个元素开始
+//	int n = 0;                               //需要查找的数据
+//	int mid = 0;
+//	scanf("%d", &n);
+//	while (left <= right)    //左闭右闭时需要加等号
+//	{
+//		mid = (left + right) / 2;
+//		if (arr[mid] < n)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > n)
+//		{
+//			right = mid - 1;  //左闭右闭需要 -1
+//		}
+//		else
+//		{
+//			break;
+//		}
+//	}
+//	if (left <= right)
+//	{
+//		printf("找到了，下标为：%d\n", mid);
+//	}
+//	else
+//	{
+//		printf("没找到!");
+//	}
+//	return 0;
+//}
+
+
+//演示多个字符串从两端向中间汇聚移动
+//#include<stdio.h>
+//#include<string.h>
+//#include<windows.h>
+//int main()
+//{
+//	char arr1[] = "hello5world";
+//	char arr2[] = "           ";
+//	int left = 0;
+//	int right = strlen(arr1) - 1;
+//	while (left <= right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];
+//		printf("%s\n", arr2);
+//		Sleep(1000);
+//		system("cls");
+//		left++;
+//		right--;
+//	}
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+
+//三次登陆输入
+#include<stdio.h>
+#include<string.h>
+int main()
 {
-	int left = 0;
-	int right = n;
-	while (left < right)        //right指向的位置没有数据，不需要加等号 [left,right)
+	char password[20] = { 0 };
+	int i = 0;
+	for (i = 0; i < 3; i++)
 	{
-		int mid = (left + right) / 2;
-		if (*(p + mid) < f)
+		printf("请输入密码：");
+		scanf("%s", password);
+		if (strcmp(password, "123456") == 0)
 		{
-			left = mid + 1;  //左边的数据去得到，所以 mid + 1
-		}
-		else if (*(p + mid) > f)
-		{
-			right = mid; //右边的数据取不到,不用-1
+			printf("登录成功");
+			break;
 		}
 		else
 		{
-			return mid;
+			printf("密码错误\n");
 		}
 	}
-	return -1;
-}
-int main()
-{
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-	int number = sizeof(arr) / sizeof(arr[0]);
-	int a = 0;
-	printf("请输入需要查找的数：");
-	scanf("%d", &a);
-	int ret = my_seek(arr, number, a);
-	if (ret != -1)
+	if (i == 3)
 	{
-		printf("找到了，下标为：%d\n", ret);
-	}
-	else
-	{
-		printf("没找到！");
+		printf("三次登录失败");
 	}
 	return 0;
 }
