263ddf368037209ce742edfe396cb094d17f400f
xwj <1983318249@qq.com>
2021-09-03 20:30:40 +0800

    2021.9.2

--- 2021_8_31/2021_8_31/test.c
+++ 2021_8_31/2021_8_31/test.c
@@ -43,28 +43,72 @@
 
 //回调函数
 //定义：通过函数指针调用的函数
-#include<stdio.h>
-int add(int x, int y)          //加法
-{
-	return x + y;
-}
-int sub(int x, int y)         //减法
-{
-	return x - y;
-}
-int my_function(int(*p)(int, int),int x,int y)
-{
-	return p(x, y);
-}
-int main()
-{
-	int a = 0;
-	int b = 0;
-	printf("请输入两个数：");
-	scanf("%d %d", &a, &b);
-	int ret = my_function(add, a, b);
-	int temp = my_function(sub, a, b);
-	printf("%d\n", ret);
-	printf("%d\n", temp);
-	return 0;
-}
+//#include<stdio.h>
+//int add(int x, int y)          //加法
+//{
+//	return x + y;
+//}
+//int sub(int x, int y)         //减法
+//{
+//	return x - y;
+//}
+//int my_function(int(*p)(int, int),int x,int y)
+//{
+//	return p(x, y);
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	printf("请输入两个数：");
+//	scanf("%d %d", &a, &b);
+//	int ret = my_function(add, a, b);
+//	int temp = my_function(sub, a, b);
+//	printf("%d\n", ret);
+//	printf("%d\n", temp);
+//	return 0;
+//}
+
+
+//冒泡排序
+//#include<stdio.h>
+//void my_sort(int arr[], int sz)          //只能排序int型的数据
+//{
+//	int i = 0;
+//	for (i = 0; i < sz - 1; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < sz - 1 - i; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int temp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = temp;
+//			}
+//		}
+//	}
+//}
+//void my_printf(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 9,8,7,6,5,4,3,2,1,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	my_sort(arr, sz);
+//	my_printf(arr, sz);
+//	return 0;
+//}
+
+
+//qsort库函数的应用 - 可以用来排序任何类型的数据（浮点数，整数，字符串）
+//void qsort( void *base,      //void* - 无具体类型指针，可以用来接收任意类型的地址
+//            size_t num, 
+//            size_t width, 
+//            int (__cdecl *compare )(const void *elem1, const void *elem2 ) );
---
+++ 2021_9_2/2021_9_2/test.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3 };
+	printf("%d\n", (*(&arr)) + 1);
+
+	return 0;
+}
