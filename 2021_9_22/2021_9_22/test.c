fe5c7acd7f454b0d9d91a48d9c44f8da6f980245
xwj <1983318249@qq.com>
2021-09-23 23:05:34 +0800

    2021.9.22

--- 2021_9_22/2021_9_22/test.c
+++ 2021_9_22/2021_9_22/test.c
@@ -1,6 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+int add(int a, int b)
+{
+	return a + b;
+}
 int main()
 {
+	int a = 3;
+	int b = 5;
+	int ret = add(a, b);
+	printf("%d\n", ret);
 	return 0;
 }
