9f658a9d5e4d0565546878db722b05035251a585
xwj <1983318249@qq.com>
2021-10-04 22:49:13 +0800

    28

---
+++ 2021_9_28/2021_9_28/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//
+//    int arr[100] = { 0 };
+//    for (int i = 0; i < n; i++)
+//    {
+//        scanf("%d", arr + i);
+//    }
+//    int num = 0;
+//    scanf("%d", &num);
+//    int m = 0;
+//    int i = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        if (arr[i] == num)
+//        {
+//            for (int j = i; j < n; j++)
+//            {
+//                arr[j] = arr[j + 1];
+//            }
+//            i--;
+//            m++;
+//        }
+//    }
+//    for (int i = 0; i < n - m; i++)
+//    {
+//        printf("%d ", arr[i]);
+//    }
+//    return 0;
+//}
+
+
