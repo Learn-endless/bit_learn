83ea7633be89bfa7989ad75b8b1fde0d6f0d5ad4
xwj <1983318249@qq.com>
2021-10-11 23:25:14 +0800

    10 11

--- 2021_10_11/2021_10_11/test.c
+++ 2021_10_11/2021_10_11/test.c
@@ -17,24 +17,47 @@
 //用宏模拟实现offsetof库函数
 //size_t offsetof( structName, memberName );
 //offsetof：求结构体变量中成员的偏移量
+//#include<stdio.h>
+//#include<stddef.h>
+//#define OFFSETOF(type_name,name) (int)&(((type_name*)0) -> name)
+//struct stu
+//{
+//	int a;      //对齐数4
+//	char b;     //对齐数1
+//	double c;   //对齐数8
+//};
+//int main()
+//{
+//	printf("%d\n", OFFSETOF(struct stu, a));
+//    printf("%d\n", OFFSETOF(struct stu, b));
+//    printf("%d\n", OFFSETOF(struct stu, c));
+//
+//	//printf("%d\n", offsetof(struct stu, a)); //offsetof的参数是类型名和变量名
+//	//printf("%d\n", offsetof(struct stu, b));
+//	//printf("%d\n", offsetof(struct stu, c));
+//
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//#define N 4
+//#define Y(n) ((N+2)*n) /*这种定义在编程规范中是严格禁止的*/
+//int main()
+//{
+//	int z = 0;
+//	z = 2 * (N + Y(5 + 1));
+//	printf("%d\n", z);
+//	return 0;
+//}
+
+
 #include<stdio.h>
-#include<stddef.h>
-#define OFFSETOF(type_name,name) (int)&(((type_name*)0) -> name)
-struct stu
-{
-	int a;      //对齐数4
-	char b;     //对齐数1
-	double c;   //对齐数8
-};
+#define A 2+2
+#define B 3+3
+#define C A*B
 int main()
 {
-	printf("%d\n", OFFSETOF(struct stu, a));
-    printf("%d\n", OFFSETOF(struct stu, b));
-    printf("%d\n", OFFSETOF(struct stu, c));
-
-	//printf("%d\n", offsetof(struct stu, a)); //offsetof的参数是类型名和变量名
-	//printf("%d\n", offsetof(struct stu, b));
-	//printf("%d\n", offsetof(struct stu, c));
-
+	printf("%d\n", C);
 	return 0;
 }
